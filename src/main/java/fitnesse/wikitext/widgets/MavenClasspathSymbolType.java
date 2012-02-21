a28bb58f1b3b77bab8d1c1ae9a60a8f8da6b6d68
Arjan Molenaar <gaphor@gmail.com>
2012-02-21 07:58:42 +0100

    Added caching.

--- src/main/java/fitnesse/wikitext/widgets/MavenClasspathSymbolType.java
+++ src/main/java/fitnesse/wikitext/widgets/MavenClasspathSymbolType.java
@@ -1,20 +1,37 @@
 package fitnesse.wikitext.widgets;
 
-import fitnesse.html.HtmlUtil;
-import fitnesse.wikitext.parser.*;
-import util.Maybe;
-
 import java.io.File;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import org.codehaus.plexus.PlexusContainerException;
+
+import util.Maybe;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+
+import fitnesse.html.HtmlUtil;
+import fitnesse.wikitext.parser.Matcher;
+import fitnesse.wikitext.parser.Parser;
+import fitnesse.wikitext.parser.Path;
+import fitnesse.wikitext.parser.PathsProvider;
+import fitnesse.wikitext.parser.Rule;
+import fitnesse.wikitext.parser.Symbol;
+import fitnesse.wikitext.parser.SymbolType;
+import fitnesse.wikitext.parser.Translation;
+import fitnesse.wikitext.parser.Translator;
 
 /**
  * FitNesse SymbolType implementation which enables Maven classpath integration for FitNesse.
  */
 public class MavenClasspathSymbolType extends SymbolType implements Rule, Translation, PathsProvider {
 
+	Cache<ParsedSymbol, List<String>> pomCache;
+	
     private MavenClasspathExtractor mavenClasspathExtractor;
 
     public MavenClasspathSymbolType() throws PlexusContainerException {
@@ -25,6 +42,16 @@ public class MavenClasspathSymbolType extends SymbolType implements Rule, Transl
 
         wikiRule(this);
         htmlTranslation(this);
+        
+		pomCache = CacheBuilder.newBuilder()
+				.expireAfterAccess(10, TimeUnit.MINUTES)
+				.build(new CacheLoader<ParsedSymbol, List<String>>() {
+
+					@Override
+					public List<String> load(ParsedSymbol key) throws Exception {
+						return mavenClasspathExtractor.extractClasspathEntries(key.getPomFile(), key.getScope());
+					}
+				});
     }
 
     @Override
@@ -41,17 +68,16 @@ public class MavenClasspathSymbolType extends SymbolType implements Rule, Transl
     }
 
 	private List<String> getClasspathElements(Symbol symbol) {
-        String pomFile = symbol.childAt(0).getContent();
-		String scope = MavenClasspathExtractor.DEFAULT_SCOPE;
+ 		
+		ParsedSymbol parsedSymbol = new ParsedSymbol(symbol.childAt(0).getContent());
 
-		if (pomFile.contains("@")) {
-        	String[] s = pomFile.split("@");
-        	pomFile = s[0];
-        	scope = s[1];
-        }
-
-		return mavenClasspathExtractor.extractClasspathEntries(new File(pomFile), scope);
+		try {
+			return pomCache.get(parsedSymbol);
+		} catch (ExecutionException e) {
+			throw new MavenClasspathExtractionException(e);
+		}
 	}
+
 
     @Override
     public Maybe<Symbol> parse(Symbol symbol, Parser parser) {
@@ -81,6 +107,64 @@ public class MavenClasspathSymbolType extends SymbolType implements Rule, Transl
 	@Override
 	public Collection<String> providePaths(Translator translator, Symbol symbol) {
 		return getClasspathElements(symbol);
+	}
+	
+	/**
+	 * Turn the pom+scope key into a comparable object, using the pom's last modified timestamp as
+	 * cache key.
+	 */
+	static class ParsedSymbol {
+		private String symbol;
+		private File pomFile;
+		private String scope;
+		private long lastModified;
+		
+		public ParsedSymbol(String symbol) {
+			super();
+			this.symbol = symbol;
+			parseSymbol();
+		}
+		
+		private void parseSymbol() {
+			if (symbol.contains("@")) {
+	        	String[] s = symbol.split("@");
+	        	pomFile = new File(s[0]);
+	        	scope = s[1];
+	        } else {
+	        	pomFile = new File(symbol);
+	        	scope = MavenClasspathExtractor.DEFAULT_SCOPE;
+	        }
+
+			lastModified = pomFile.lastModified();
+		}
+		
+		public String getSymbol() {
+			return symbol;
+		}
+		
+		public File getPomFile() {
+			return pomFile;
+		}
+		
+		public String getScope() {
+			return scope;
+		}
+
+		/* hashCode() and equals() are optimized for used in the cache */
+		
+		@Override
+		public int hashCode() {
+			return symbol.hashCode() + (int) lastModified;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (obj instanceof ParsedSymbol) {
+				ParsedSymbol ps = (ParsedSymbol) obj;
+				return symbol.equals(ps.symbol) && lastModified == ps.lastModified;
+			}
+			return false;
+		}
 	}
 }
 
